7c54062a403e77fae14677588f9df51331c340e1
Dylan Gardham <message4dylan@gmail.com>
2024-03-12 20:04:30 +0000

    sdl2 window

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use sdl2::{event::Event, pixels::Color, keyboard::Keycode};
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("Ray marcher? I hardly know her!", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(100, 149, 237));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..}
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+    }
+
+    return Ok(());
+}
